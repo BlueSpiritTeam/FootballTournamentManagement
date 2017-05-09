8fa4873073be69fed18dc1c17f169db1b1240da4
louisnguyen2602 <14521167@gm.uit.edu.vn>
2017-05-09 21:25:22 +0700

    Create SoureCode DTO

---
+++ SourceCode/SourceCode/DTO/PlayerDTO.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceCode.DTO
+{
+    class PlayerDTO
+    {
+        private string player_id;
+        private uint role_id;
+        private uint goal_num;
+        private uint assist_num;
+        private uint kit_num;
+        public string Player_id
+        {
+            get
+            {
+                return player_id;
+            }
+
+            set
+            {
+                player_id = value;
+            }
+        }
+
+        public uint Role_id
+        {
+            get
+            {
+                return role_id;
+            }
+
+            set
+            {
+                role_id = value;
+            }
+        }
+
+        public uint Goal_num
+        {
+            get
+            {
+                return goal_num;
+            }
+
+            set
+            {
+                goal_num = value;
+            }
+        }
+
+        public uint Assist_num
+        {
+            get
+            {
+                return assist_num;
+            }
+
+            set
+            {
+                assist_num = value;
+            }
+        }
+
+        public uint Kit_num
+        {
+            get
+            {
+                return kit_num;
+            }
+
+            set
+            {
+                kit_num = value;
+            }
+        }
+
+        public PlayerDTO(){ }
+
+        public PlayerDTO(string pid, uint rid,  uint gnum, uint anum, uint knum)
+        {
+            this.Player_id = pid;
+            this.Role_id = rid;
+            this.Goal_num = gnum;
+            this.Assist_num = anum;
+            this.Kit_num = knum;
+        }
+
+        public PlayerDTO(DataRow row)
+        {
+            this.Player_id = (string)row["PlayerID"].ToString();
+            this.Role_id = uint.Parse(row["RoleID"].ToString());
+            this.Goal_num = uint.Parse(row["GoalNumber"].ToString());
+            this.Assist_num = uint.Parse(row["AssistNumber"].ToString());
+            this.Kit_num = uint.Parse(row["KitNum"].ToString());
+        }
+    }
+}
